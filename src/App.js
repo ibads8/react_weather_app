63ea3687e09d41d92929c2ef8aac61e3b53690aa
ibads8 <ibads8@users.noreply.github.com>
2022-06-21 10:35:19 -0600

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by Eu and is
+        <a href="https://github.com/ibads8/react_weather_app" target="_blank">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
