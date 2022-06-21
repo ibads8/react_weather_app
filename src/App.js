d496b89ef3646c5b34ef17fc30d69dbba98f49cc
ibads8 <ibads8@users.noreply.github.com>
2022-06-21 13:17:56 -0600

    Updated react scripts

--- src/App.js
+++ src/App.js
@@ -8,10 +8,22 @@ function App() {
       <h1> Weather App</h1>
       <footer>
         {" "}
-        This project was coded by Eu and is
-        <a href="https://github.com/ibads8/react_weather_app" target="_blank">
+        This project was coded by Eu and is{" "}
+        <a
+          href="https://github.com/ibads8/react_weather_app"
+          target="_blank"
+          rel="noreferrer"
+        >
           {" "}
           open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://app.netlify.com/sites/comfy-tapioca-39b8d1/overview"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
         </a>
       </footer>
     </div>
