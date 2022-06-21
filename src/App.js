1331a5f51caff1a1a5c9a6095820f0f22599569c
ibads8 <ibads8@users.noreply.github.com>
2022-06-21 16:07:01 -0600

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by Eu and is{" "}
-        <a
-          href="https://github.com/ibads8/react_weather_app"
-          target="_blank"
-          rel="noreferrer"
-        >
+      <div className="container">
+        <Weather />
+        <footer>
           {" "}
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://app.netlify.com/sites/comfy-tapioca-39b8d1/overview"
-          target="_blank"
-          rel="noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+          This project was coded by Eu and is{" "}
+          <a
+            href="https://github.com/ibads8/react_weather_app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://app.netlify.com/sites/comfy-tapioca-39b8d1/overview"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
